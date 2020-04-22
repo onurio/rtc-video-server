5f7e8abfe8b4077ea2184786bd9af4e0f54ef98a
onurio <omrinuri@gmail.com>
2020-04-22 17:00:50 -0500

    minor syntax iprovmentes

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const app = require('express')();
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-// const PORT = process.env.PORT || 4000;
-// const SocketManager = require('./SocketManager');
-
-
-
-// server.listen(PORT,()=>console.log('listetning on: '+PORT));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', SocketManager);
-
-
-
-
-
 
 var app = require('http').createServer();
 var io = module.exports.io = require('socket.io')(app);
